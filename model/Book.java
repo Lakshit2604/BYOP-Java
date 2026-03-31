052720a382a83f4c87d1bb1e8f1ec60548e178b6
Lakshit-G <lakshitg27@gmail.com>
2026-03-31 12:07:02 +0530

    Add model folder

---
+++ model/Book.java
@@ -0,0 +1,31 @@
+package model;
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
+
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isAvailable() { return available; }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-10s %-25s %-20s %-10s", 
+                             id, title, author, (available ? "Available" : "Issued"));
+    }
+}
